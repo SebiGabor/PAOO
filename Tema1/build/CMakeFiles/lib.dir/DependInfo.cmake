54e553d2f30816f187295520a5328c2821b6be94
SebiGabor <sebastian.gabor@student.upt.ro>
2023-11-01 23:36:18 +0200

    Made the structure

---
+++ Tema1/build/CMakeFiles/lib.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sebi/Desktop/PAOO/Teme/PAOO/Tema1/src/DNA.cpp" "/home/sebi/Desktop/PAOO/Teme/PAOO/Tema1/build/CMakeFiles/lib.dir/src/DNA.cpp.o"
+  "/home/sebi/Desktop/PAOO/Teme/PAOO/Tema1/src/Population.cpp" "/home/sebi/Desktop/PAOO/Teme/PAOO/Tema1/build/CMakeFiles/lib.dir/src/Population.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Tema1/src/main.cpp
@@ -0,0 +1,8 @@
+#include "DNA.hpp"
+#include "Population.hpp"
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello world!\n";
+}
